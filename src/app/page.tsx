8dce96c80761fa520b8c6ffbba249a9127988bfc
Dmitry Kuchukov <kudim@inbox.ru>
2024-06-07 16:32:09 +0300

    feat: add base css classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="container">
-      <h1 className="text-center text-3xl text-black-600">BeautySchools</h1>
+    <div className="container mx-auto">
+      <h1>Заголовок H1</h1>
+      <h2>Заголовок H2 </h2>
+      <h3>Заголовок H3</h3>
+      <h4>Заголовок H4</h4>
+      <p>Основной текст</p>
     </div>
   )
 }
